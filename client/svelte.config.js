dedfa1271e5813006865449a214ad4e00c9d6704
Phyrone <phyrone@phyrone.de>
2023-04-22 14:25:45 +0200

    gh actions update
    + some client preperations

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,18 +1,51 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import staticAdapter from "@sveltejs/adapter-static";
+import cloudflareAdapter from "@sveltejs/adapter-cloudflare";
+import {vitePreprocess} from '@sveltejs/kit/vite';
+
+
+/** @type {boolean} */
+const isTauri = process.env.TAURI_PLATFORM !== undefined
+const BUILD_TARGET_DEFAULT = "static"
+const BUILD_TARGET_TAURI = BUILD_TARGET_DEFAULT
+
+/** @type { "static" | "cloudflare" } */
+const buildTarget = isTauri ? BUILD_TARGET_TAURI : (process.env.BUILD_TARGET || BUILD_TARGET_DEFAULT).toLowerCase()
+
+/** @type {import('@sveltejs/kit').Adapter} */
+function getAdapter() {
+    switch (buildTarget) {
+        case "static":
+            return staticAdapter({
+                pages: "dist",
+                assets: "dist",
+                precompress: !isTauri,
+                fallback: isTauri ? "index.html" : "404.html",
+                //fallback: "index.html",
+            })
+        case "cloudflare":
+            return cloudflareAdapter()
+        default:
+            throw new Error(`Unknown build target: ${buildTarget}`)
+    }
+
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: getAdapter(),
+        serviceWorker: {
+            register: false
+        }
+    }
 };
 
 export default config;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,49 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vitest/config';
+
+function getTarget() {
+    switch (process.env.TAURI_PLATFORM) {
+        // Tauri supports es2021
+        case "windows":
+            return "chrome105";
+        case "macos":
+        case "linux":
+            return "safari13";
+
+        default: //not tauri or unknown target
+            return "es2020";
+    }
+}
+
+const IS_TAURI = process.env.TAURI_PLATFORM !== undefined
+const TAURI_DEBUG = process.env.TAURI_DEBUG === "true"
+
+function getMinify() {
+    return TAURI_DEBUG ? false : IS_TAURI ? "esbuild" : (import.meta?.env?.PROD === true) ? "esbuild" : false;
+}
+
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    plugins: [sveltekit()],
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    // prevent vite from obscuring rust errors
+    clearScreen: false,
+    // tauri expects a fixed port, fail if that port is not available
+    server: {
+        port: 1420,
+        strictPort: true,
+    },
+
+    test: {
+        include: ['src/**/*.{test,spec}.{js,ts}']
+    },
+    envPrefix: ["VITE_", "TAURI_"],
+    build: {
+        target: getTarget(),
+        // don't minify for debug builds
+        minify: getMinify(),
+        // produce sourcemaps for debug builds
+        sourcemap: !getMinify(),
+        assetsDir: "static",
+    },
 });
